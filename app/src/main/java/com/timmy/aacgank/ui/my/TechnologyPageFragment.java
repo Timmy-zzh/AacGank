1eebf1beaab7545a404a757f8117910e0e082cb6
timmy1 <timmy_zzh@163.com>
2018-12-28 23:37:46 +0800

    UI绘制流程

--- app/src/main/java/com/timmy/aacgank/ui/my/TechnologyPageFragment.java
+++ app/src/main/java/com/timmy/aacgank/ui/my/TechnologyPageFragment.java
@@ -97,12 +97,13 @@ public class TechnologyPageFragment extends TPageLazyBaseFragment<ViewRecyclerVi
                 pageListOne.add(new MainModel(MainTag.ANDROID.TAG_CONTENTPROVIDER, "ContentProvider"));
 
                 pageListOne.add(new MainModel(MainTag.ANDROID.TAG_MOTION_EVENT, "事件分发"));
+                pageListOne.add(new MainModel(MainTag.ANDROID.TAG_UI_DRAW, "UI绘制流程"));
+                pageListOne.add(new MainModel(MainTag.ANDROID.TAG_PAINT, "图形绘制:Canvas,Paint,Path使用"));
+
+                pageListOne.add(new MainModel(MainTag.ANDROID.TAG_ANIMATION, "动画"));
+                pageListOne.add(new MainModel(MainTag.ANDROID.TAG_DATA_BASE, "数据存储"));
 
                 pageListOne.add(new MainModel(MainTag.ANDROID.TAG_BEHAVIOR, "Behavior"));
-                pageListOne.add(new MainModel(MainTag.ANDROID.TAG_DATA_BASE, "数据存储"));
-                pageListOne.add(new MainModel(MainTag.ANDROID.TAG_UI_DRAW, "UI绘制流程"));
-                pageListOne.add(new MainModel(MainTag.ANDROID.TAG_PAINT, "绘图操作:Canvas,Path与Paint使用"));
-                pageListOne.add(new MainModel(MainTag.ANDROID.TAG_ANIMATION, "动画"));
                 pageListOne.add(new MainModel(MainTag.ANDROID.TAG_RECYCLER_VIEW, "RecycleView解析"));
 
 //                pageListOne.add(new MainModel(MainTag.ANDROID.TAG_COLLAPSING_TOOLBAR_LAYOUT, "CollapsingToolbarLayout使用"));
